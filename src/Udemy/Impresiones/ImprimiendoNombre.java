b523b268bc5ad1b026152b97dda9d428942d2396
Ing-FernandoEstrada <ferchoestrada1998@gmail.com>
2023-09-02 16:47:32 -0500

    4:47 pm

---
+++ src/Udemy/Impresiones/ImprimiendoNombre.java
@@ -0,0 +1,15 @@
+package Udemy;
+
+public class ImprimiendoNombre
+{
+    public static void main(String[] args)
+    {
+        //Imprimiendo nombre directamente
+        System.out.println("Fernando Estrada");
+
+        //Estariamos creando una variable de tipo String con el nombre
+        String nombre = "Fernando Estrada";
+        //Estariamos concatenando el texto mas el nombre
+        System.out.println("Hola! mi nombre es: " + nombre);
+    }
+}
